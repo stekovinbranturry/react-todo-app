e6402e7812d7f34f7751c0576fb5e5245f87f5f2
ZHANG Kai <kai.zhang@soprasteria.com>
2019-07-18 12:21:16 +0800

    another options: useReducer

--- react-hooks-with-react-redux/src/components/TodoList.js
+++ react-hooks-with-react-redux/src/components/TodoList.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useReducer } from 'react';
 import { List, Checkbox, Icon } from 'antd';
-import { toggleTodoAction, removeTodoAction } from '../reducers/reducer';
+import {
+  toggleTodoAction,
+  removeTodoAction,
+  initState,
+  reducer
+} from '../reducers/reducer';
 
 export default () => {
-	const dispatch = useDispatch();
-	const todos = useSelector(state => state.todos);
-	const handleClickOnCheckbox = id => {
-		dispatch(toggleTodoAction(id));
-	};
-	const handleClickOnIcon = id => {
-		dispatch(removeTodoAction(id));
-	};
-	return (
-		<div id="show_area">
-			<List
-				bordered
-				dataSource={todos}
-				renderItem={(item, index) => (
-					<div>
-						<List.Item
-							style={
-								item.isComplete ? { textDecoration: 'line-through' } : null
-							}
-						>
-							<Checkbox
-								checked={item.isComplete}
-								onClick={handleClickOnCheckbox.bind(null, item.id)}
-							/>
-							<span>{item.todo}</span>
-							<Icon
-								className="delete-icon"
-								onClick={handleClickOnIcon.bind(null, item.id)}
-								type="delete"
-							/>
-						</List.Item>
-					</div>
-				)}
-			/>
-		</div>
-	);
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { todos } = state;
+  const handleClickOnCheckbox = id => {
+    dispatch(toggleTodoAction(id));
+  };
+  const handleClickOnIcon = id => {
+    dispatch(removeTodoAction(id));
+  };
+  return (
+    <div id='show_area'>
+      <List
+        bordered
+        dataSource={todos}
+        renderItem={item => (
+          <div>
+            <List.Item
+              style={
+                item.isComplete ? { textDecoration: 'line-through' } : null
+              }
+            >
+              <Checkbox
+                checked={item.isComplete}
+                onClick={handleClickOnCheckbox.bind(null, item.id)}
+              />
+              <span>{item.todo}</span>
+              <Icon
+                className='delete-icon'
+                onClick={handleClickOnIcon.bind(null, item.id)}
+                type='delete'
+              />
+            </List.Item>
+          </div>
+        )}
+      />
+    </div>
+  );
 };
